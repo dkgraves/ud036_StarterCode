178c21f4fa1cff727a87247c4af2ed4960109154
Graves, David (dg3017) <dg3017@att.com>
2018-11-25 17:22:40 -0500

    [Feature-001] Added additional comments in the media.py file to be more concise.

--- Movies/media.py
+++ Movies/media.py
@@ -2,7 +2,20 @@ import webbrowser
 
 
 class Movie():
-    """ This class provides a way to store movie related information."""
+    """ This class provides a way to store movie related information.
+    Parameters
+    -------------
+    movie_title : str
+        This is the title of the movie.
+    movie_rating : str
+        This is the movie industry rating, ie G, PG, PG-13, R.
+    movie_storyLine : str
+        A short description of the movie.
+    poster_image : str
+        This is the url to an image of the movie poster or other artwork.
+    trailer_youtube_url : str
+        url to the youtube movie trailer
+    """
     # VALID_RATINGS = ["G", "PG", "PG-13", "R"]
     def __init__(self, movie_title, movie_rating, movie_storyline,
                  poster_image, trailer_youtube):
@@ -14,5 +27,7 @@ class Movie():
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
-        """ This method within Movie() opens a window and plays the trailer."""
+        """ This method within Movie() opens a window and plays the trailer
+        that was supplied  in the trailer_youtube argument.
+        """
         webbrowser.open(self.trailer_youtube_url)
